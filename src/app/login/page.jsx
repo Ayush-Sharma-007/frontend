de8c9d5a7a94ccd1d788967e2d70e3491ee2b7e6
Ayush-Sharma-007 <ayush120410sharma@gmail.com>
2024-09-05 19:05:52 +0530

    uploading files

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,59 +1,43 @@
-'use client'
 import React from 'react';
 import classes from './login.module.css';
-import Input from '@/components/input';
+import Input from '@/components/Input';
 import Button from '@/components/Button';
-import * as Yup from 'yup';
 import { useFormik } from 'formik';
-
-const loginSchema = Yup.object().shape({
-  email : Yup.string()
-    .email("Invalid Email")
-    .required("Required"),
-
-})
 
 const Login = () => {
 
   const loginForm = useFormik({
-      initialValues: {
-        email : '',
-        password : '',
-      },
-
-      onSubmit: (values) => {
-        console.log(values);
-      },
-      validationSchema : loginSchema
-  });
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    }
+  })
 
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <div className='w-full max-w-md mx-auto'>
-        <div className='rounded-lg border-2 shadow p-8'>
-          <h1 className='uppercase font-bold text-center my-6 text-gray-600 text-3xl'>Login</h1>
+    <div>
+      <h1 className='text-center text-2xl font-bold'>Login Page</h1>
+      <button className='btn'>Login button</button>
+      <button className={classes.myBtn}>Module CSS</button>
 
-          <form onSubmit={ loginForm.handleSubmit }>
+      <form onSubmit={loginForm.handleSubmit}>
 
-            <label htmlFor="email">Email Address</label>
-            <span className='text-sm ml-4 '>{ loginForm.touched.email && loginForm.errors.email}</span>
+        <input id={'email'} onChange={loginForm.handleChange}
+          value={loginForm.values.email} type={'email'} />
 
-            <input id="email" type="email" onChange={ loginForm.handleChange } value={ loginForm.values.email } className='w-full border px-2 py-2 rounded mb-4' />
+        <input id={'password'} onChange={loginForm.handleChange}
+          value={loginForm.values.email} type={'password'} />
 
-            <label htmlFor="password">Password</label>
-            <span className='text-sm ml-4 '>{ loginForm.touched.password && loginForm.errors.password}</span>
-            <input id="password" type="password" onChange={ loginForm.handleChange } value={ loginForm.values.password } className='w-full border px-2 py-2 rounded mb-4' />
+        <button type='submit'>Submit</button>
+      
+      </form>
 
-            <input type="checkbox"/>
-            <span className='ml-1'>Remember me</span>
+      <Button>Login</Button>
 
-            <button className='w-full border bg-blue-500 px-3 py-2 rounded-md text-white mt-4 '>Login</button>
-
-          </form>
-        </div>
-      </div>
     </div>
   )
 }
 
-export default Login
+export default Login;
--- src/app/updateuser/[id]/page.jsx
+++ src/app/updateuser/[id]/page.jsx
@@ -1,85 +1,91 @@
-'use client'
-import axios from 'axios';
-import { Formik } from 'formik';
-import { useParams, useRouter } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
-import toast from 'react-hot-toast';
+"use client";
+import axios from "axios";
+import { Formik } from "formik";
+import { useParams, useparams, useRouter } from "next/navigation";
+import { Result } from "postcss";
+import React, { useEffect } from "react";
+import toast from "react-hot-toast";
 
 const UpdateUser = () => {
+  const { id } = useParams();
+  const [userData, setUserData] = React.useState(null);
+  const router = useRouter();
 
-    const { id } = useParams();
-    const [userData, setuserData] = useState(null) // null is used to make it easier to apply the condition
-    const router = useRouter();         // to redirect the site after updation
-
-    const getUserData = async () => {
-        const res = await axios.get('http://localhost:5000/user/getbyid/' + id);
-        console.log(res.data);
-        setuserData(res.data);
-    }
+  const getUserData = async () => {
+    const res = await axios.get("http://localhost:5000/user/getbyid/" + id);
+    console.log(res.data);
+    setUserData(res.data);
+  };
 
     useEffect(() => {
-        getUserData();
-    }, [])
+    getUserData();
+  }, []);
 
-
-    const submitForm = (values) => {
-        console.log(values);
-
-        axios.put('http://localhost:5000/user/update/'+id, values)
-        .then((result) => {
-            toast.success('User Updated Successfully')
-            router.push('/manageuser')          // this line redirects the page to manageuser after successfull updation
-        }).catch((err) => {
-            console.log(err);
-            toast.error('Failed to update User')           
-        });
-    }
-
-    return (
-        <div className='max-w-xl mx-auto p-5 border rounded-lg shadow'>
-            <h1 className='text-2xl text-center font-bold'>Update User Details</h1>
-
-            {
-                userData !== null ? (
-
-                    <Formik initialValues={userData} onSubmit={submitForm}>
-                        {
-                            (updateForm) => {
-                                return (
-                                    <form onSubmit={ updateForm.handleSubmit}>
-
-                                        <input className='w-full p-3 border rounded-lg mt-5 bg-gray-100' 
-                                        onChange={updateForm.handleChange} 
-                                        value={updateForm.values.name} 
-                                        id='name' type="text" placeholder='Enter Your Name' />
-
-                                        <input className='w-full p-3 border rounded-lg mt-5 bg-gray-100' 
-                                        onChange={updateForm.handleChange} 
-                                        value={updateForm.values.email} 
-                                        id='email' type="email" placeholder='Enter Your Email' />
-
-                                        <input className='w-full p-3 border rounded-lg mt-5 bg-gray-100' 
-                                        onChange={updateForm.handleChange} 
-                                        value={updateForm.values.password} 
-                                        id='password' type="password" placeholder='Enter Your Password' />
-
-                                        <input className='w-full p-3 border rounded-lg mt-5 bg-gray-100' 
-                                        onChange={updateForm.handleChange} 
-                                        value={updateForm.values.city} 
-                                        id='city' type="text" placeholder='Enter Your City' />
-
-                                        <button className='block ml-auto p-3 bg-blue-500 text-white mt-6 rounded-lg'>Update User</button>
-                                    </form>
-                                )
-                            }
-                        }
-                    </Formik>
-
-                ) : <p className='text-3xl text-gray-300 text-center font-bold mt-5'>Loading...</p>
-            }
-
-        </div>
-    )
+  const submitform = (values) => {
+    console.log(values);
+    axios.put("http://localhost:5000/user/update" +id,values)
+    .then((result)=>{
+        toast.success('User Update Successfully')
+        router.push('/manageuser')
+    }) .catch((err) => {
+        toast.error('failed to update user')
+    });
 }
+  return (
+    <div>
+      <div className="max-w-xl mx-auto p-5 border mt-5 shadow">
+        <h1 className="text-2xl text-ceneter font-bold">Update User Detail</h1>
+        {userData !== null ? (
+          <Formik initialValues={userData} onsubmit={submitform}>
+            {(updateForm) => {
+              return (
+                <form onsubmit={updateForm.handleSubmit}>
+                  <input
+                    className="w-full p-3 border rounded-lg mt-5 bg-gray-100"
+                    placeholder="Enter your name"
+                    onChange={updateForm.handleChange}
+                    value={updateForm.values.name}
+                    type="name"
+                    id='name'
+                  />
+                  <input
+                    className="w-full p-3 border rounded-lg mt-5 bg-gray-100"
+                    placeholder="Enter your Email"
+                    onChange={updateForm.handleChange}
+                    value={updateForm.values.email}
+                    type="email"
+                    id='email'
+                  />
+                  <input
+                    className="w-full p-3 border rounded-lg mt-5 bg-gray-100"
+                    placeholder="Enter your Password"
+                    onChange={updateForm.handleChange}
+                    value={updateForm.values.password}
+                    type="password"
+                    id ="password"
+                  />
+                  <input
+                    className="w-full p-3 border rounded-lg mt-5 bg-gray-100"
+                    placeholder="Enter your City"
+                    onChange={updateForm.handleChange}
+                    value={updateForm.values.city}
+                    type="city"
+                    id='city'
+                  />
+                  <button className="block ml-auto p-3 bg-blue-500 text-white mt-6 rounded-lg"> Submit </button>
+                </form>
+              );
+            }}
+          </Formik>
+        ) : (
+          <p className="text-3xl text-gray-100 text-center font-bold mt-5">
+            Loading...
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default UpdateUser
+
+export default UpdateUser;
